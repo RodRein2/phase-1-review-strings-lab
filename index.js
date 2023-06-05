4e6d2d6d7ddec7226412c0667218340acb386ad8
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-06-05 15:16:39 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Winnafred Beatrice Gertrude';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.charAt[0]}!`;
